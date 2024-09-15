e56e94d0ed05313f88d422eb5a7a200b1cb5167f
Harshit Agarwal <harshitagarwalwork21@gmail.com>
2024-09-16 01:02:15 +0530

    loannetwork changes

--- Qubes/settings.py
+++ Qubes/settings.py
@@ -160,3 +160,23 @@ MESSAGE_TAGS = {
 }
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': 'Qubes/error.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
+
+
