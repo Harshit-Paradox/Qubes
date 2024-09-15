bab5a3b0cddddd205ac9ee77d56460507684ba98
Harshit Agarwal <harshitagarwalwork21@gmail.com>
2024-09-16 01:25:07 +0530

    remove error

--- Qubes/settings.py
+++ Qubes/settings.py
@@ -160,23 +160,3 @@ MESSAGE_TAGS = {
 }
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'ERROR',
-            'class': 'logging.FileHandler',
-            'filename': 'Qubes/error.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    },
-}
-
-
